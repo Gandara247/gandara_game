3c863022cd8208e6b9403d31a569c572ecc98962
Gandara <gustavogandarasilva@gmail.com>
2023-10-27 09:13:00 -0300

    feat: added moves

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         if (this.position.y + this.height + this.velocity.y < canvas.height)
             this.velocity.y += gravity
@@ -37,7 +38,16 @@ const player = new Player({
 const player2 = new Player({
     x: 300,
     y: 100,
-})
+});
+
+const keys = {
+    d: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+};
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -46,8 +56,68 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     player2.update()
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 1
+    else if(keys.a.pressed) player.velocity.x = -1
 
 
 };
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "d":
+            keys.d.pressed = true
+            break
+        case "a":
+            keys.a.pressed = true
+            break
+        case "w":
+            player.velocity.y = -20
+            break
+        case "s":
+            player.velocity.y = 1
+            break
+    }
+});
+
+
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case "d":
+            keys.d.pressed = false
+            break
+        case "a":
+            keys.a.pressed = false
+            break
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
