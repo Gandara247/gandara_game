d26e3fce4fb35dd2a831bae466b2d8021258e651
Gandara <gustavogandarasilva@gmail.com>
2023-10-27 17:53:06 -0300

    feat: added new files

--- index.js
+++ index.js
@@ -4,53 +4,33 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-const scaledCancas= {
-    width:canvas.width /4,
-    height: canvas.height /4,
-}
-const gravity = 0.5;
-
-class Sprite {
-    constructor({ position, imageSrc }) {
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    };
-
-    draw() {
-        if (!this.image) return
-        c.drawImage(this.image, this.position.x, this.position.y)
-    };
-
-    update() {
-        this.draw()
-    }
-}
-
-class Player {
-    constructor(position) {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1,
-        }
-        this.height = 100;
-    };
-
-    draw() {
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    };
-
-    update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else this.velocity.y = 0
-    };
+const scaledCancas = {
+    width: canvas.width / 4,
+    height: canvas.height / 4,
 };
+
+const FloorCollisions2D = []
+
+for (let i = 0; i < FloorCollisions.length; i += 36) {
+    FloorCollisions2D.push(FloorCollisions.slice(i, i + 36))
+};
+
+const collisionBlocks = []
+FloorCollisions2D.forEach((row) => {
+    row.forEach((symbol) => {
+        if (symbol === 202) {
+            console.log("draw a block here!!!");
+            collisionBlocks.push(new CollisionBlock({
+                position: {
+                    x: 0,
+                    y: 0,
+                }
+            }))
+        }
+    })
+})
+
+const gravity = 0.5;
 
 const player = new Player({
     x: 0,
